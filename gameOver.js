42e81ecacb0fa7b43f5415a721ac72ff84c1679d
Jay <179924362+CedricSmith00@users.noreply.github.com>
2024-09-20 12:39:23 +0100

    Thirst and hunger decrease on sleep being pressed. Thirst and Tiredness now decrease when feed is pressed.

--- gameOver.js
+++ gameOver.js
@@ -31,6 +31,7 @@ const hideAllActions = () => {
     wolf.style.display = 'none'
     polarBear.style.display = 'none'
     snowLeopard.style.display = 'none'
+    subheading.style.display = 'none'
 }
 
 const hideAllStats = () => {
@@ -134,28 +135,31 @@ const increaseHealth = (amount) => {
 
 // Functionality for feed, sleep, and drink buttons
 feed.addEventListener("click", () => {
-    hungervalue = Math.min(hungervalue + 10, 100) // Increase hunger level by 10, max 100
+    hungervalue = Math.min(hungervalue + 5, 100) // Increase hunger level by 5, max 100
     hunger.value = hungervalue 
-    increaseHealth(5) // Increase health by 5 when feeding
+    thirstvalue = Math.max(thirstvalue - 3, 0) // Lose 3 thirst when feeding
+    increaseHealth(4) // Increase health by 5 when feeding
 })
 
 sleep.addEventListener("click", () => {
-    tirednessvalue = Math.min(tirednessvalue + 10, 100) // Increase tiredness level by 10, max 100
+    tirednessvalue = Math.min(tirednessvalue + 5, 100) // Increase tiredness level by 5, max 100
     tiredness.value = tirednessvalue 
-    increaseHealth(5) // Increase health by 5 when sleeping
+    hungervalue = Math.max(hungervalue - 4, 0) // Lose 4 hunger when sleeping
+    thirstvalue = Math.max(thirstvalue - 4, 0) // Lose 4 thirst when sleeping
+    increaseHealth(4) // Increase health by 5 when sleeping
 })
 
 drink.addEventListener("click", () => {
-    thirstvalue = Math.min(thirstvalue + 10, 100) // Increase thirst level by 10, max 100
+    thirstvalue = Math.min(thirstvalue + 5, 100) // Increase thirst level by 5, max 100
     thirst.value = thirstvalue 
-    increaseHealth(4) // Increase health by 5 when drinking
+    increaseHealth(4) // Increase health by 4 when drinking
 })
 
 // Functionality for hunt, fish, and howl buttons
 hunt.addEventListener("click", () => {
     hungervalue = Math.max(hungervalue - 5, 0); // Decrease hunger by 5
-    thirstvalue = Math.max(thirstvalue - 5, 0); // Decrease thirst by 3
-    tirednessvalue = Math.max(tirednessvalue - 5, 0); //Decrease tiredness by 5
+    thirstvalue = Math.max(thirstvalue - 5, 0); // Decrease thirst by 5
+    tirednessvalue = Math.max(tirednessvalue - 5, 0); // Decrease tiredness by 5
     hunger.value = hungervalue; 
     thirst.value = thirstvalue; 
 });
@@ -170,7 +174,7 @@ fish.addEventListener("click", () => {
 
 howl.addEventListener("click", () => {
     hungervalue = Math.max(hungervalue - 3, 0); // Decrease hunger by 3
-    thirstvalue = Math.max(thirstvalue - 3, 0); //Decrease thirst by 3
+    thirstvalue = Math.max(thirstvalue - 3, 0); // Decrease thirst by 3
     tirednessvalue = Math.max(tirednessvalue - 5, 0); // Decrease tiredness by 5
     hunger.value = hungervalue; 
     tiredness.value = tirednessvalue; 
